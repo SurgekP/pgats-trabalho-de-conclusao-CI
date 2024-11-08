adf5825b36b0dc9bb86d0d6bac14d382ecda5039
SurgekP <paloma.ocardozo@gmail.com>
2024-11-07 21:32:24 -0300

    ci - atualizacao da workflows com relatorios

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'cypress/reports/html/.jsons',
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
     baseUrl:'https://automationexercise.com',
     setupNodeEvents(on, config) {
